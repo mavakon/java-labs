498e28211eaeab94b5be54aa5d64dc81e53398ef
mavakon <maksim.m.konshin@gmail.com>
2023-11-02 16:37:24 +0200

    [LAB2] Step 2

--- lab2/app/src/main/java/lab2/Matrix.java
+++ lab2/app/src/main/java/lab2/Matrix.java
@@ -5,5 +5,30 @@ public class Matrix {
     private int cols;
     private double[][] data;
 
+    // Пуста матриця
+    public Matrix() {
+        this.rows = 0;
+        this.cols = 0;
+        this.data = new double[0][0];
+    }
+
+    // Матриця заданого розміру
+    public Matrix(int rows, int cols) {
+        this.rows = rows;
+        this.cols = cols;
+        this.data = new double[rows][cols];
+    }
+
+    // Копія іншої матриці
+    public Matrix(Matrix matrix) {
+        this.rows = matrix.rows;
+        this.cols = matrix.cols;
+        this.data = new double[rows][cols];
+
+        for (int i = 0; i < rows; i++) {
+            System.arraycopy(matrix.data[i], 0, this.data[i], 0, cols);
+        }
+    }
+
 }
 
